0346f65559e668dc1bd6ac9a09705981f33c0cca
Paralitik <sergeylobodanov@gmail.com>
2023-01-10 21:39:14 +0300

    fix lint errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,41 @@
-import { useEffect, useState } from "react";
-import { Form } from './components/Form/Form'
-import { MessageList } from './components/MessageList/MessageList'
-import style from './App.module.scss'
+import { useEffect, useState } from 'react';
+import { Form } from './components/Form/Form';
+import { MessageList } from './components/MessageList/MessageList';
+import style from './App.module.scss';
 
 export const App = () => {
   const [messageList, setMessageList] = useState([]);
 
-  const addToMessageList = (newMessage)  => {
-    setMessageList((prevMessage)=> [...prevMessage, newMessage])
-  }
+  const addToMessageList = (newMessage) => {
+    setMessageList((prevMessage) => [...prevMessage, newMessage]);
+  };
 
   const botAnswer = {
     id: Math.random() * 1000,
     text: 'Hello',
-    author: 'BOT'
-  }
+    author: 'BOT',
+  };
 
-  useEffect(()=> {
-    if(messageList.length > 0 && messageList[messageList.length - 1].author !== 'BOT') {
-      const timer = setTimeout(()=> {
-        addToMessageList(botAnswer)
-      }, 1500)
+  useEffect(() => {
+    if (
+      messageList.length > 0 &&
+      messageList[messageList.length - 1].author !== 'BOT'
+    ) {
+      const timer = setTimeout(() => {
+        addToMessageList(botAnswer);
+      }, 1500);
       return () => {
-        clearTimeout(timer)
-      }
+        clearTimeout(timer);
+      };
     }
-  }, [messageList])
+  }, [messageList]);
 
-  return <>
-    <div className={style.container}>
-      <Form addNewMessage={addToMessageList}/>
-      <MessageList messageList={messageList}/>
-    </div>
-  </>
+  return (
+    <>
+      <div className={style.container}>
+        <Form addNewMessage={addToMessageList} />
+        <MessageList messageList={messageList} />
+      </div>
+    </>
+  );
 };
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,27 +1,41 @@
-import { useEffect, useState } from 'react'
-import style from './Form.module.scss'
+import { useState } from 'react';
+import style from './Form.module.scss';
 
 export const Form = ({ addNewMessage }) => {
-    const [messageText, setMessageText] = useState('')
-    const [messageAuthor, setMessageAuthor] = useState('')
+  const [messageText, setMessageText] = useState('');
+  const [messageAuthor, setMessageAuthor] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const message = {
-            id: Math.random() * 1000,
-            text: messageText,
-            author: messageAuthor
-        }
-        addNewMessage(message)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const message = {
+      id: Math.random() * 1000,
+      text: messageText,
+      author: messageAuthor,
+    };
+    addNewMessage(message);
+  };
 
-    return <>
-        <div className={style.wrp}>
-            <form className={style.form} onSubmit={handleSubmit}>
-                <input className={style.input} type="text" name="author" placeholder="author" onChange={(e) => setMessageAuthor(e.target.value)} />
-                <input className={style.input} type="text" name="message" placeholder="message" onChange={(e) => setMessageText(e.target.value)} />
-                <button className={style.btn}>Send message</button>
-            </form>
-        </div>
+  return (
+    <>
+      <div className={style.wrp}>
+        <form className={style.form} onSubmit={handleSubmit}>
+          <input
+            className={style.input}
+            type="text"
+            name="author"
+            placeholder="author"
+            onChange={(e) => setMessageAuthor(e.target.value)}
+          />
+          <input
+            className={style.input}
+            type="text"
+            name="message"
+            placeholder="message"
+            onChange={(e) => setMessageText(e.target.value)}
+          />
+          <button className={style.btn}>Send message</button>
+        </form>
+      </div>
     </>
-}
+  );
+};
